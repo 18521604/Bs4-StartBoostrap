a715eeb7ef3a92cec2d4b57c4b6042783b7b5b4a
18521604 <18521604@gm.uit.edu.vn>
2020-04-10 18:14:16 +0700

    completed the Project

--- index.js
+++ index.js
@@ -20,25 +20,67 @@ $(function () {
     $('.navbar ul li:nth-child(1) a').click(function(e){
         $('html,body').animate({scrollTop:$('#services').offset().top - menuHeight},1500, "easeInOutExpo");
         return false;
-    })
+    });
     $('.navbar ul li:nth-child(2) a').click(function(e){
         $('html,body').animate({scrollTop:$('#portfolio').offset().top - menuHeight},1500, "easeInOutExpo");
         return false;
-    })
+    });
     $('.navbar ul li:nth-child(3) a').click(function(e){
         $('html,body').animate({scrollTop:$('#about').offset().top - menuHeight},1500, "easeInOutExpo");
         return false;
-    })
+    });
     $('.navbar ul li:nth-child(4) a').click(function(e){
         $('html,body').animate({scrollTop:$('#team').offset().top - menuHeight},1500, "easeInOutExpo");
         return false;
-    })
+    });
     $('.navbar ul li:nth-child(5) a').click(function(e){
         $('html,body').animate({scrollTop:$('#contact').offset().top - menuHeight},1500, "easeInOutExpo");
         return false;
-    })
+    });
+
+    // Hover menu of navbar
+    $(window).scroll(function(){
+        $('.navbar ul li a').removeClass('active');
+        if (($(this).scrollTop() >= $('#services').offset().top - menuHeight) &&
+            ($(this).scrollTop() < $('#portfolio').offset().top - menuHeight)) {
+            $('.navbar ul li:nth-child(1) a').addClass('active');
+        } else {
+            $('.navbar ul li a').removeClass('active');
+            if (($(this).scrollTop() >= $('#portfolio').offset().top - menuHeight) &&
+                ($(this).scrollTop() < $('#about').offset().top - menuHeight)) {
+                $('.navbar ul li:nth-child(2) a').addClass('active');
+            } else {
+                $('.navbar ul li a').removeClass('active');
+                if (($(this).scrollTop() >= $('#about').offset().top - menuHeight) &&
+                    ($(this).scrollTop() < $('#team').offset().top - menuHeight)) {
+                    $('.navbar ul li:nth-child(3) a').addClass('active');
+                } else {
+                    $('.navbar ul li a').removeClass('active');
+                    if (($(this).scrollTop() >= $('#team').offset().top - menuHeight) &&
+                        ($(this).scrollTop() < $('#contact').offset().top - menuHeight)) {
+                        $('.navbar ul li:nth-child(4) a').addClass('active');
+                    } else {
+                        $('.navbar ul li a').removeClass('active');
+                        if (($(this).scrollTop() >= $('#contact').offset().top - menuHeight)) {
+                            $('.navbar ul li:nth-child(5) a').addClass('active');
+                        } else {
+                            $('.navbar ul li a').removeClass('active');
+                        };
+                    };
+                };
+            };
+        };
+    });
 
     //Button up top
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('.btn-up').fadeIn('slow');
+        } else{
+            $('.btn-up').fadeOut('slow');
+        }
+    });
+
     $('.btn-up').click(function(){
         $('html,body').animate({scrollTop:0},1500,"easeInOutExpo");
     })
